259780647d04b4de121f50faee69c2d3546a0858
mdubkov <mdubkov@icerockdev.com>
2022-06-03 10:02:07 +0700

    #39 fixes after review

--- fields-core/src/commonMain/kotlin/dev/icerock/moko/fields/core/validations/ValidationBuilders.kt
+++ fields-core/src/commonMain/kotlin/dev/icerock/moko/fields/core/validations/ValidationBuilders.kt
@@ -1,15 +1,10 @@
+/*
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package dev.icerock.moko.fields.core.validations
 
 import dev.icerock.moko.resources.desc.StringDesc
-
-@Deprecated(
-    message = "Deprecated in favor of new DSL-like fieldValidation function",
-    level = DeprecationLevel.WARNING,
-    replaceWith = ReplaceWith("fieldValidation", "dev.icerock.moko.fields.core.validations")
-)
-fun <D> fieldValidationBlock(
-    block: ValidationResult<D>.() -> ValidationResult<D>
-): ((D) -> StringDesc?) = { block(ValidationResult.of(it)).validate() }
 
 fun <D> fieldValidation(
     block: ValidationResult<D>.() -> ValidationResult<D>
--- fields-livedata/src/commonMain/kotlin/dev/icerock/moko/fields/livedata/validations/ValidationBuilders.kt
+++ fields-livedata/src/commonMain/kotlin/dev/icerock/moko/fields/livedata/validations/ValidationBuilders.kt
@@ -6,19 +6,9 @@ package dev.icerock.moko.fields.livedata.validations
 
 import dev.icerock.moko.fields.core.validations.ValidationResult
 import dev.icerock.moko.fields.core.validations.fieldValidation
-import dev.icerock.moko.fields.core.validations.fieldValidationBlock
 import dev.icerock.moko.fields.livedata.liveBlock
 import dev.icerock.moko.mvvm.livedata.LiveData
 import dev.icerock.moko.resources.desc.StringDesc
-
-@Deprecated(
-    message = "Deprecated in favor of new DSL-like fieldValidation function",
-    level = DeprecationLevel.WARNING,
-    replaceWith = ReplaceWith("fieldValidation", "dev.icerock.moko.fields.livedata.validations")
-)
-fun <D> fieldValidationBlock(
-    block: ValidationResult<D>.() -> ValidationResult<D>
-): ((LiveData<D>) -> LiveData<StringDesc?>) = liveBlock(fieldValidationBlock(block))
 
 fun <D> fieldValidation(
     block: ValidationResult<D>.() -> ValidationResult<D>
